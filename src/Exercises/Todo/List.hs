21082f8cd9a1917678cabff054bc61a719747cea
George Wilson <george@wils.online>
2019-01-22 14:17:09 +1000

    todoList

--- src/Exercises/Todo/List.hs
+++ src/Exercises/Todo/List.hs
@@ -7,6 +7,7 @@ Portability : non-portable
 -}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE TupleSections #-}
 module Exercises.Todo.List where
 
 import Control.Lens
@@ -22,14 +23,36 @@ import Reflex.Dom.Core
 import Common.Todo
 import Exercises.Todo.Item
 
+addItem :: MonadWidget t m => m (Event t TodoItem)
+addItem =
+  el "div" $ mdo
+    ti <- textInput $ def & textInputConfig_setValue .~ ("" <$ eValue)
+    let
+      dValue = ti ^. textInput_value
+      dStripped = Text.strip <$> dValue
+      eEnter = keypress Enter ti
+      eValue = ffilter (not . Text.null) (current dValue <@ eEnter)
+    pure (TodoItem False <$> eValue)
+
+
 todoListExercise :: MonadWidget t m
                  => [TodoItem]
                  -> m ()
-todoListExercise tis =
+todoListExercise items = mdo
+  eAdd <- addItem
+  eAdd' <- numberOccurrencesFrom (length items) (Just <$> eAdd)
+  let
+    eRemove' = fmap (,Nothing) eRemove
+    eAddRemove = leftmost [eAdd', eRemove']
+    m = Map.fromList . zip [0..] $ items
+    eNewMap = ffor eAddRemove $ \(n,mti) -> n =: mti
+  dMap <- listHoldWithKey m eNewMap $ \_ item ->
+    todoItem item
+  let eRemove = switchDyn $ fmap (leftmost . fmap (uncurry (<$)) . Map.toList . fmap snd) dMap
   pure ()
 
 todoListModelExercise :: MonadWidget t m
                       => [TodoItem]
                       -> m (Dynamic t [TodoItem])
-todoListModelExercise tis =
+todoListModelExercise items =
   pure (pure [])
